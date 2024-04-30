353ca3a8969c00ec1d73e9fa2c86ea5c89d3a13b
mohini-442 <mohinikarwasra598@gmail.com>
2024-04-29 15:54:08 -1200

    co

--- src/app/components/Headersec.jsx
+++ src/app/components/Headersec.jsx
@@ -8,7 +8,7 @@ const Headersec = () => {
                 <div className='max-w-7xl mx-auto px-3'>
                     <div className='flex flex-row flex-wrap -mx-3'>
                         <div className='lg:w-1/2 w-full flex justify-center flex-col'>
-                            <h2 className='text-5xl ff-sora font-normal leading-[66px]'>Hello I&apos;am <span className='font-extrabold'> Flora Sheen. Frontend</span> <span className='text-white outline-[3px] p-4 outline-black'>Developer</span> Based In <span className='font-extrabold'>India.</span></h2>
+                            <h2 className='text-5xl ff-sora font-normal leading-[66px]'>Hello I&apos;am <span className='font-extrabold'> Mohini. Frontend</span> <span className='text-white  text-stroke-3 text-5xl ff-sora font-extrabold'>Developer</span> Based In <span className='font-extrabold'>India.</span></h2>
                             <p className='text-base text-[#71717A] pt-5 max-w-[544px]'>I&apos;m Flora Sheen Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.</p>
 
                         </div>
--- src/app/components/Myskills.jsx
+++ src/app/components/Myskills.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { myarray } from '../common/helper'
+import Marquee from "react-fast-marquee";
 
 const Myskills = () => {
     return (
-        <div className='max-w-7xl mx-auto px-3 pt-20'>
+        <div className='max-w-7xl mx-auto px-3 pt-20 py-44'>
             <h2 className='text-5xl font-normal text-center'>My <span className='font-extrabold'>Skills</span> </h2>
-            <div className='flex flex-wrap gap-12 pt-12'>
+            {/* <div className='flex flex-wrap gap-12 pt-12'>
                 {myarray.map((data, index) => (
                     <div key={index}>
                         <div className='w-[186px] h-[186px] rounded-[4px] border-2 border-black flex justify-center items-center flex-col'>
@@ -15,7 +15,28 @@ const Myskills = () => {
                     </div>
                 ))}
 
-            </div>
+            </div> */}
+
+
+            <Marquee>
+                <div className='w-[200px]'>
+                    <img src="/assets/images/html.png" alt=""  className='w-[120px] h-[120px]'/>
+                </div>
+                <div className='w-[200px]'>
+                    <img src="/assets/images/css.png" alt="" className='w-[100px] h-[100px]' />
+                </div>
+                <div className='w-[200px]'>
+                    <img src="/assets/images/js.png" alt="" className='w-[80px] h-[80px]' />
+                </div>
+                <div className='w-[200px]'>
+                    <img src="/assets/images/tailwindcss.jpg" alt="" className='w-[60px] h-[40px]'/>
+                </div>
+                <div className='w-[200px]'>
+                    <img src="/assets/images/git.png" alt="" className='w-[80px] h-[80px]' />
+                </div>
+            </Marquee>
+
+
         </div>
     )
 }
